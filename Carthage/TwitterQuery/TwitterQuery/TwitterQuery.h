3eac9969f59b483cc5aa696ddc504e43cfb69b78
🦊Tomoya Hirano <noppelabs@gmail.com>
2017-12-08 22:23:02 +0900

    support carthage

---
+++ Carthage/TwitterQuery/TwitterQuery/TwitterQuery.h
@@ -0,0 +1,19 @@
+//
+//  TwitterQuery.h
+//  TwitterQuery
+//
+//  Created by Tomoya Hirano on 2017/12/08.
+//  Copyright © 2017年 Tomoya Hirano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TwitterQuery.
+FOUNDATION_EXPORT double TwitterQueryVersionNumber;
+
+//! Project version string for TwitterQuery.
+FOUNDATION_EXPORT const unsigned char TwitterQueryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TwitterQuery/PublicHeader.h>
+
+
